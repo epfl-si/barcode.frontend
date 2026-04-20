5e8a61d20f0933f8d741e5dbf232baf2487582ef
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-20 17:00:05 +0200

    [fix] Read environment variables correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {LocalStorageBackend, OIDCContext} from "@epfl-si/react-appauth";
-import {env} from "./lib/env.ts";
 
 const store = new LocalStorageBackend();
-const environment = env();
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <div>
     <OIDCContext
-      authServerUrl={environment.REACT_APP_AUTH_SERVER_URL ?? ''}
+      authServerUrl={import.meta.env.LIL_REACT_APP_AUTH_SERVER_URL ?? ''}
       client={{
-        clientId: environment.OIDC_CLIENT_ID ?? '',
-        scope: environment.OIDC_SCOPE,
+        clientId: import.meta.env.LIL_OIDC_CLIENT_ID ?? '',
+        scope: import.meta.env.LIL_OIDC_SCOPE,
         redirectUri: window.location.href
       }}
       storage={store}
       refreshStorage={window.localStorage}
-      onLogout={() => window.location.href=environment.REACT_APP_HOMEPAGE_URL ?? ''}
+      onLogout={() => window.location.href=import.meta.env.LIL_REACT_APP_HOMEPAGE_URL ?? ''}
     >
       <App />
     </OIDCContext>
-  </StrictMode>,
+  </div>,
 )
