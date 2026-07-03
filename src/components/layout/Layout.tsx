19f2d5c56df4e0a7a4cefdae1778f3c51518e419
Rosa Maggi <rosa.maggi@epfl.ch>
2026-07-03 11:13:29 +0200

    [feature] Add environment

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,14 +3,19 @@ import {Footer} from "@/components/layout/Footer.tsx";
 import {Outlet} from "react-router";
 import type {UserType} from "@/lib/types.tsx";
 import type {State} from "@epfl-si/react-appauth";
+import {env} from "@/lib/env.tsx";
+import {useTranslation} from "react-i18next";
 
 export const Layout = ({ user, oidc }: { user: UserType, oidc: State }) => {
+  const { t } = useTranslation();
 
   return (
     <main className="flex flex-col h-screen">
       <Header user={user} onLogin={() => oidc.login()}
               onLogout={() => oidc.logout()}/>
       <div className="p-4 sm:p-8 w-full 2xl:max-w-[90%] 3xl:max-w-[80%] mx-auto flex-1">
+        {env().ENVIRONMENT !== 'prod' && <div className="environment">
+          {t('environment', { environment: env().ENVIRONMENT ?? 'LOCAL' })}</div>}
         <Outlet />
       </div>
       <Footer />
