fababe16fe0ed0a036592984ec93c83e057ced68
Rosa Maggi <rosa.maggi@epfl.ch>
2026-08-25 16:36:55 +0200

    [fix] Message form synchronization

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,6 +5,7 @@ import type {UserType} from "@/lib/types.tsx";
 import type {State} from "@epfl-si/react-appauth";
 import {env} from "@/lib/env.tsx";
 import {useTranslation} from "react-i18next";
+import {Info} from "lucide-react";
 
 export const Layout = ({ user, oidc }: { user: UserType, oidc: State }) => {
   const { t } = useTranslation();
@@ -16,6 +17,12 @@ export const Layout = ({ user, oidc }: { user: UserType, oidc: State }) => {
       <div className="p-4 sm:p-8 w-full 2xl:max-w-[90%] 3xl:max-w-[80%] mx-auto flex-1">
         {env().ENVIRONMENT !== 'prod' && <div className="environment">
           {t('environment', { environment: env().ENVIRONMENT ?? 'LOCAL' })}</div>}
+
+        <div className="gap-2 flex flex-row mb-5 border-gray-200 border-1 p-2 font-bold justify-center items-center" style={{ color: "#4a90e2", fontSize: "large" }}>
+          <Info style={{ width: "40px", height: "40px", color: "#4a90e2" }}/>
+          {t("app.infoSavingDetails")}
+        </div>
+
         <Outlet />
       </div>
       <Footer />
--- src/components/pages/BarcodeDetailPage.tsx
+++ src/components/pages/BarcodeDetailPage.tsx
@@ -1,6 +1,6 @@
 import {Link, useParams} from "react-router";
 import {useEffect, useState} from "react";
-import {ArrowLeft, QrCode as BarcodeIcon, Info, ShelvingUnit, Trash2} from "lucide-react";
+import {ArrowLeft, QrCode as BarcodeIcon, ShelvingUnit, Trash2} from "lucide-react";
 import type {State} from "@epfl-si/react-appauth";
 import {useTranslation} from 'react-i18next';
 import {Details} from "@/components/form/Details.tsx";
@@ -110,11 +110,6 @@ export const BarcodeDetailPage = ({ oidc, connectedUser }: { oidc: State, connec
             {t('app.backToStorage')}
           </Link>
         </Button>
-
-        <div className="gap-2 flex flex-row align-middle mb-5 border-gray-200 border-1 p-2">
-          <Info className="text-blue-500"/>
-          {t("app.infoSavingDetails")}
-        </div>
 
         <div className={`flex items-center gap-3 mb-6 ${details?.deletedBy ? 'line-through opacity-50' : ''}`}>
           <div className="bg-[#f0f0f0] rounded-[12px] h-10 w-10 flex items-center justify-center">
